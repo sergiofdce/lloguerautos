cd0f71edcb5b3b359ea2e40bb4d28cf14337c206
Enric Mieza <emieza@xtec.cat>
2023-04-21 22:15:02 +0200

    model

--- lloguer/models.py
+++ lloguer/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 
 class Automobil(models.Model):
-	marca = models.CharField(max_length=50)
-	model = models.CharField(max_length=50)
-	matricula = models.CharField(max_length=10)
-
+    marca = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    matricula = models.CharField(max_length=10)
+    def __str__(self):
+        return self.matricula + ' - ' + self.marca + " " + self.model
 
--- lloguerautos/settings.py
+++ lloguerautos/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lloguer.apps.LloguerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
