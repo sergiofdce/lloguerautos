ff48c03a7cd49527776ffeb76c61c15675d06a74
Enric Mieza <emieza@xtec.cat>
2025-03-27 20:47:01 +0100

    rewind

--- lloguer/models.py
+++ lloguer/models.py
@@ -6,6 +6,4 @@ class Automobil(models.Model):
     marca = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     matricula = models.CharField(max_length=10)
-    def __str__(self):
-        return self.matricula + ' - ' + self.marca + " " + self.model
 
